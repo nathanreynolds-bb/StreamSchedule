611d280baa6088d610d1c632bc3d15f4709f5c1a
nathanreynolds-bb <101427661+nathanreynolds-bb@users.noreply.github.com>
2025-05-17 19:52:04 -0500

    Refactor Schedule component layout and styles for improved structure and readability

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -8,15 +8,20 @@ const schedule = [
 
 export default function Schedule() {
     return (
-        <main id="grid-wrapper">
-            <h1 className="grid-item">Stream Schedule:</h1>
-            {schedule.map((entry, index) => (
-                <React.Fragment key={index}>
-                    <div className="grid-item">{entry.day}:</div>
-                    <div className="grid-item">{entry.time}</div>
-                    <div className="grid-item">{entry.game}</div>
-                </React.Fragment>
-            ))}
-        </main>
+        <div id="background">
+            <main id="grid-wrapper">
+                <h1 className="grid-item heading">Stream Schedule:</h1>
+                    <h2 className="grid-item label day">Day</h2>
+                    <h2 className="grid-item label time">Time</h2>
+                    <h2 className="grid-item label game">Game</h2>
+                {schedule.map((entry, index) => (
+                    <React.Fragment key={index}>
+                        <div className="grid-item day">{entry.day}:</div>
+                        <div className="grid-item time">{entry.time}</div>
+                        <div className="grid-item game">{entry.game}</div>
+                    </React.Fragment>
+                ))}
+            </main>
+        </div>
     );
 }
